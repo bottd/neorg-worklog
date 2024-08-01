ce55cd1e7ebfc08568f49bf53e5c1cc030fe01c1
Drake Bott <bott.da@gmail.com>
2024-08-01 14:58:51 -0500

    fix: use hidden window for editing worklog

--- lua/neorg/modules/external/worklog/module.lua
+++ lua/neorg/modules/external/worklog/module.lua
@@ -37,103 +37,111 @@ module.load = function()
 end
 
 module.log_norg_file = function(event)
-  dirman.set_closest_workspace_match()
   local workspace = dirman.get_workspace_match()
   local meta = treesitter.get_document_metadata()
-  local winview = vim.fn.winsaveview()
 
   if workspace == "default" then
     workspace = module.config.public.default_workspace_title
   end
 
-  vim.api.nvim_command(":Neorg journal today")
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = vim.api.nvim_open_win(buf, false, {
+    relative = "editor",
+    width = 1,
+    height = 1,
+    row = 0,
+    col = 0,
+    hide = true
+  })
 
-  local bufnr = vim.api.nvim_get_current_buf()
-  local journal_path = vim.api.nvim_buf_get_name(bufnr)
+  vim.api.nvim_win_call(win, function()
+    vim.api.nvim_command(":Neorg journal today")
+    local bufnr = vim.api.nvim_get_current_buf()
+    local journal_path = vim.api.nvim_buf_get_name(bufnr)
 
-  -- do not log journal to it's own worklog
-  if event.file == journal_path then
-    return
-  end
+    -- do not log journal to it's own worklog
+    if event.file == journal_path then
+      return
+    end
 
-  local worklog_title_line = nil
-  local workspace_title_line = nil
+    local worklog_title_line = nil
+    local workspace_title_line = nil
 
-  local worklog_title_tmpl = [[
-    ((heading1 title: (paragraph_segment) @title)
-      (#eq? @title "%s"))
-  ]]
-
-  treesitter.execute_query(
-    string.format(worklog_title_tmpl, module.config.public.heading),
-    function(query, id, node, metadata)
-      worklog_title_line = treesitter.get_node_range(node).row_start
-    end,
-    bufnr
-  )
-
-  if worklog_title_line ~= nil then
-    local workspace_title_tmpl = [[
-      ((heading2 title: (paragraph_segment) @workspace)
-        (#eq? @workspace "%s"))
+    local worklog_title_tmpl = [[
+      ((heading1 title: (paragraph_segment) @title)
+        (#eq? @title "%s"))
     ]]
+
     treesitter.execute_query(
-      string.format(workspace_title_tmpl, workspace),
+      string.format(worklog_title_tmpl, module.config.public.heading),
       function(query, id, node, metadata)
-        local text = treesitter.get_node_text(node)
-        workspace_title_line = treesitter.get_node_range(node).row_start
+        worklog_title_line = treesitter.get_node_range(node).row_start
       end,
       bufnr
     )
-  end
 
-  -- Check if file already in worklog before insert lines
-  if workspace_title_line ~= nil then
-    local file_in_worklog = false
-    -- escape special characters in file name for search
-    local escaped_file = event.file:gsub("([^%w])", "%%%1")
-    treesitter.execute_query(
-      "(unordered_list1 content: (paragraph) @content)",
-      function(query, id, node, metadata)
-        local text = treesitter.get_node_text(node)
-        if string.match(text, escaped_file) then
-          file_in_worklog = true
-          return true
-        end
-    end, 
-    bufnr)
-
-    if file_in_worklog then
-      -- early return, no insert needed
-      vim.api.nvim_buf_delete(bufnr, {})
-      vim.fn.winrestview(winview)
-      return
+    if worklog_title_line ~= nil then
+      local workspace_title_tmpl = [[
+        ((heading2 title: (paragraph_segment) @workspace)
+          (#eq? @workspace "%s"))
+      ]]
+      treesitter.execute_query(
+        string.format(workspace_title_tmpl, workspace),
+        function(query, id, node, metadata)
+          local text = treesitter.get_node_text(node)
+          workspace_title_line = treesitter.get_node_range(node).row_start
+        end,
+        bufnr
+      )
     end
-  end
 
-  local lines = {"   - {:" .. event.file .. ":}[" .. (meta.title or event.file) .. "]"}
+    -- Check if file already in worklog before insert lines
+    if workspace_title_line ~= nil then
+      local file_in_worklog = false
+      -- escape special characters in file name for search
+      local escaped_file = event.file:gsub("([^%w])", "%%%1")
+      treesitter.execute_query(
+        "(unordered_list1 content: (paragraph) @content)",
+        function(query, id, node, metadata)
+          local text = treesitter.get_node_text(node)
+          if string.match(text, escaped_file) then
+            file_in_worklog = true
+            return true
+          end
+      end, 
+      bufnr)
 
-  if workspace_title_line == nil then
-    table.insert(lines, 1, "** " .. workspace)
-  end
+      if file_in_worklog then
+        -- early return, no insert needed
+        vim.api.nvim_buf_delete(bufnr, {})
+        return
+      end
+    end
 
-  if worklog_title_line == nil then
-    table.insert(lines, 1, "* " .. module.config.public.heading)
-  end
+    local lines = {"   - {:" .. event.file .. ":}[" .. (meta.title or event.file) .. "]"}
+
+    if workspace_title_line == nil then
+      table.insert(lines, 1, "** " .. workspace)
+    end
+
+    if worklog_title_line == nil then
+      table.insert(lines, 1, "* " .. module.config.public.heading)
+    end
 
 
-  if workspace_title_line ~= nil then
-    vim.api.nvim_buf_set_lines(bufnr, workspace_title_line + 1, workspace_title_line + 1, false, lines)
-  elseif worklog_title_line ~= nil then
-    vim.api.nvim_buf_set_lines(bufnr, worklog_title_line + 1, worklog_title_line + 1, false, lines)
-  else
-    local line_count = vim.api.nvim_buf_line_count(bufnr)
-    vim.api.nvim_buf_set_lines(bufnr, line_count, line_count, false, lines)
-  end
+    if workspace_title_line ~= nil then
+      vim.api.nvim_buf_set_lines(bufnr, workspace_title_line + 1, workspace_title_line + 1, false, lines)
+    elseif worklog_title_line ~= nil then
+      vim.api.nvim_buf_set_lines(bufnr, worklog_title_line + 1, worklog_title_line + 1, false, lines)
+    else
+      local line_count = vim.api.nvim_buf_line_count(bufnr)
+      vim.api.nvim_buf_set_lines(bufnr, line_count, line_count, false, lines)
+    end
 
-  vim.cmd('silent! write')
-  vim.api.nvim_buf_delete(bufnr, {})
-  vim.fn.winrestview(winview)
+    vim.cmd('silent! write')
+    vim.api.nvim_buf_delete(bufnr, {})
+  end)
 end
 
 return module
+
